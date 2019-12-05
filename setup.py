28fc047a0c605ae38cb01eb3fe7fee4c7e8114db
Sarios <danielhp95@gmail.com>
2019-12-05 00:44:00 +0000

    bumped 0.1 -> 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='gym_kuhn_poker',
-      version='0.1',
+      version='0.2',
       description='OpenAI gym environment for Kuhn poker',
       url='https://github.com/Danielhp95/gym-kuhn-poker',
       author='Sarios',
